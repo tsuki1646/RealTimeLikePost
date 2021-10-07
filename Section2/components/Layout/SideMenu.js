c5bd70145099db671a632520de7f77f4156cef6d
tsuki1646 <aoanhtrang_16@yahoo.com>
2021-10-07 17:33:58 +0900

    Section8: Lesson 90,91 Making Responsive
    Section8: Lesson 90,91 Making Responsive

--- Section2/components/Layout/SideMenu.js
+++ Section2/components/Layout/SideMenu.js
@@ -1,78 +1,92 @@
-import React from 'react';
+import React from "react";
 import { List, Icon } from "semantic-ui-react";
 import Link from "next/link";
-import { useRouter } from 'next/router';
-import {logoutUser} from "../../utils/authUser"
+import { useRouter } from "next/router";
+import { logoutUser } from "../../utils/authUser";
 
-const SideMenu = ({user: {unreadNotification, email, unreadMessage, username}}) => {
-    const router = useRouter();
-    const isActive = route=>router.pathname===route;
-    return (
-        <>
-            <List style={{ paddingTop: "1rem"}}
-                size = "big"
-                verticalAlign="middle"
-                selection
-            >
-                <Link href="/">
-                    <List.Item active={isActive('/')}>
-                        <Icon className="home" size="large" color={isActive('/') && 'teal'}/>
-                        <List.Content>
-                            <List.Header content="Home" />
-                        </List.Content>
-                    </List.Item>
-                </Link>
-                <br />
+const SideMenu = ({
+  user: { unreadNotification, email, unreadMessage, username },
+  pc = true,
+}) => {
+  const router = useRouter();
+  const isActive = (route) => router.pathname === route;
+  return (
+    <>
+      <List
+        style={{ paddingTop: "1rem" }}
+        size="big"
+        verticalAlign="middle"
+        selection
+      >
+        <Link href="/">
+          <List.Item active={isActive("/")}>
+            <Icon
+              className="home"
+              size="large"
+              color={isActive("/") && "teal"}
+            />
+            <List.Content>{pc && <List.Header content="Home" />}</List.Content>
+          </List.Item>
+        </Link>
+        <br />
 
-                <Link href="/messages">
-                    <List.Item active={isActive("/messages")} as="a" href="/messages">
-                        <Icon
-                        name={unreadMessage ? "hand point right" : "mail outline"}
-                        size="large"
-                        color={(isActive("/messages") && "teal") || (unreadMessage && "orange")}
-                        />
-                        <List.Content>{<List.Header content="Messages" />}</List.Content>
-                    </List.Item>
-                </Link>
-                <br />
+        <Link href="/messages">
+          <List.Item active={isActive("/messages")} as="a" href="/messages">
+            <Icon
+              name={unreadMessage ? "hand point right" : "mail outline"}
+              size="large"
+              color={
+                (isActive("/messages") && "teal") || (unreadMessage && "orange")
+              }
+            />
+            <List.Content>
+              {pc && <List.Header content="Messages" />}
+            </List.Content>
+          </List.Item>
+        </Link>
+        <br />
 
-                <Link href="/notifications">
-                    <List.Item active={isActive('/notifications')}>
-                        <Icon 
-                            className={unreadNotification ? "hand point right": "bell outline"} 
-                            size="large" 
-                            color={
-                                (isActive('/notifications') && 'teal') || (unreadNotification && "orange")
-                                }/>
-                        <List.Content>
-                            <List.Header content="Notifications" />
-                        </List.Content>
-                    </List.Item>
-                </Link>
-                <br />
+        <Link href="/notifications">
+          <List.Item active={isActive("/notifications")}>
+            <Icon
+              className={
+                unreadNotification ? "hand point right" : "bell outline"
+              }
+              size="large"
+              color={
+                (isActive("/notifications") && "teal") ||
+                (unreadNotification && "orange")
+              }
+            />
+            <List.Content>
+              {pc && <List.Header content="Notifications" />}
+            </List.Content>
+          </List.Item>
+        </Link>
+        <br />
 
-                <Link href={`/${username}`}>
-                    <List.Item active={router.query.username === username}>
-                        <Icon className="user" size="large" color={router.query.username === username && 'teal'}/>
-                        <List.Content>
-                            <List.Header content="Account" />
-                        </List.Content>
-                    </List.Item>
-                </Link>
-                <br />
+        <Link href={`/${username}`}>
+          <List.Item active={router.query.username === username}>
+            <Icon
+              className="user"
+              size="large"
+              color={router.query.username === username && "teal"}
+            />
+            <List.Content>
+              {pc && <List.Header content="Account" />}
+            </List.Content>
+          </List.Item>
+        </Link>
+        <br />
 
-               
-                <List.Item onClick={() =>logoutUser(email)}>
-                    <Icon className="log out" size="large" />
-                    <List.Content>
-                        <List.Header content="Logout" />
-                    </List.Content>
-                </List.Item>
-                <br />
-                
-            </List>
-        </>
-    );
-}
+        <List.Item onClick={() => logoutUser(email)}>
+          <Icon className="log out" size="large" />
+          <List.Content>{pc && <List.Header content="Logout" />}</List.Content>
+        </List.Item>
+        <br />
+      </List>
+    </>
+  );
+};
 
 export default SideMenu;
--- Section2/pages/_app.js
+++ Section2/pages/_app.js
@@ -5,59 +5,55 @@ import baseUrl from "../utils/baseUrl";
 import { redirectUser } from "../utils/authUser";
 import Layout from "../components/Layout/Layout";
 import "react-toastify/dist/ReactToastify.css";
-import 'semantic-ui-css/semantic.min.css'
+import "semantic-ui-css/semantic.min.css";
 
-function MyApp({Component, pageProps}){
-    return(
-        <Layout {...pageProps}>
-            <Component {...pageProps} />
-        </Layout>
-    );
+function MyApp({ Component, pageProps }) {
+  return (
+    <Layout {...pageProps}>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-MyApp.getInitialProps = async({Component, ctx})=>{
-    const { token } = parseCookies(ctx);
-    let pageProps = {};
+MyApp.getInitialProps = async ({ Component, ctx }) => {
+  const { token } = parseCookies(ctx);
+  let pageProps = {};
 
-    const protectedRoutes = 
-        ctx.pathname === "/" ||
-        ctx.pathname === "/[username]" ||
-        ctx.pathname === "/notifications" ||
-        ctx.pathname === "/post/[postId]" ||
-        ctx.pathname === "/messages" ||
-        ctx.pathname === "/search"
-    ;
-
-    if (!token) 
-    {
-        protectedRoutes && redirectUser(ctx, "/login");
-    }
-    //
-    else 
-    {
-        if (Component.getInitialProps) {
-            pageProps = await Component.getInitialProps(ctx);
-        }
-
-        try {
-            const res = await axios.get(`${baseUrl}/api/auth`, {
-            headers: { Authorization: token }
-            });
-
-            const { user, userFollowStats } = res.data;
-
-            if (user) !protectedRoutes && redirectUser(ctx, "/");
-
-            pageProps.user = user;
-            pageProps.userFollowStats = userFollowStats;
-        } catch (error) {
-            destroyCookie(ctx, "token");
-            redirectUser(ctx, "/login");
-        }
+  const protectedRoutes =
+    ctx.pathname === "/" ||
+    ctx.pathname === "/[username]" ||
+    ctx.pathname === "/notifications" ||
+    ctx.pathname === "/post/[postId]" ||
+    ctx.pathname === "/messages" ||
+    ctx.pathname === "/search";
+  if (!token) {
+    protectedRoutes && redirectUser(ctx, "/login");
+  }
+  //
+  else {
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
     }
 
-    console.log('PageProps:' + pageProps);
-    return { pageProps };
-}
+    try {
+      const res = await axios.get(`${baseUrl}/api/auth`, {
+        headers: { Authorization: token },
+      });
+
+      const { user, userFollowStats } = res.data;
+
+      if (user) !protectedRoutes && redirectUser(ctx, "/");
+
+      pageProps.user = user;
+      pageProps.userFollowStats = userFollowStats;
+    } catch (error) {
+      destroyCookie(ctx, "token");
+      redirectUser(ctx, "/login");
+    }
+  }
+
+  console.log("PageProps:" + pageProps);
+  return { pageProps };
+};
 
 export default MyApp;
