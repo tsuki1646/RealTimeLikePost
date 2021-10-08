3c8a32337f84247d231935dde6cb6f3daa54ca20
tsuki1646 <aoanhtrang_16@yahoo.com>
2021-10-08 16:50:46 +0900

    Session 11: Heroku
    Session 11: Heroku

--- Section2/utils/baseUrl.js
+++ Section2/utils/baseUrl.js
@@ -1,3 +1,6 @@
-const baseUrl ="http://localhost:3000";
+const baseUrl =
+  process.env.NODE_ENV !== "production"
+    ? "http://localhost:3000"
+    : "https://twitter-mini-app.herokuapp.com";
 
 module.exports = baseUrl;
